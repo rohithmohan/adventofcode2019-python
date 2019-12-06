5578e9ebac8977d975e0f13ad43c92ce74098d92
Rohith Mohan <Rohith.MOHAN@3ds.com>
2019-12-05 17:48:18 -0800

    Day 5 part 2 solution passed

--- Day 5/code.py
+++ Day 5/code.py
@@ -20,18 +20,16 @@ def evaluateMode(x, iteration, increment, paramDict):
         return x[x[iteration + increment]]
 
 
-def evaluateCode(x):
+def evaluateCode(x, inputval):
     i = 0
     while i < len(x):
         if len(str(x[i])) > 1:
             opCode = int(str(x[i])[-2:])
-            print("opcode:", opCode)
+
             paramDict = {1: 0, 2: 0, 3: 0}
             for paramCount in range(len(str(x[i])) - 2):
                 paramPosition = -(3 + paramCount)
                 paramDict[paramCount + 1] = int(str(x[i])[paramPosition])
-                print("paramDict", paramDict)
-                print("paramCount:",paramCount)
 
         else:
             paramDict = {1: 0, 2: 0, 3: 0}
@@ -40,32 +38,57 @@ def evaluateCode(x):
         if opCode == 99:
             break
         elif opCode == 1:
-            # x[x[i + 3]] = x[x[i + 1]] + x[x[i + 2]]
             x[x[i + 3]] = evaluateMode(x, i, 1, paramDict) + evaluateMode(
                 x, i, 2, paramDict)
             i = i + 4
         elif opCode == 2:
-            # x[x[i + 3]] = x[x[i + 1]] * x[x[i + 2]]
             x[x[i + 3]] = evaluateMode(x, i, 1, paramDict) * evaluateMode(
                 x, i, 2, paramDict)
             i = i + 4
         elif opCode == 3:
-            print("Input required, passing 1 to input")
-            x[x[i + 1]] = 1
+            x[x[i + 1]] = inputval
             i = i + 2
         elif opCode == 4:
-            print("Outputting: " + str(x[x[i + 1]]))
+            print("x[i]:", x[i], "len(x):", len(x), "i+1:", i + 1, "x[i+1]:",
+                  str(x[i + 1]))
+            print("Outputting: " + str(evaluateMode(x, i, 1, paramDict)))
             i = i + 2
-
+        elif opCode == 5:
+            if (evaluateMode(x, i, 1, paramDict) != 0):
+                i = evaluateMode(x, i, 2, paramDict)
+            else:
+                i = i + 3
+        elif opCode == 6:
+            if (evaluateMode(x, i, 1, paramDict) == 0):
+                i = evaluateMode(x, i, 2, paramDict)
+            else:
+                i = i + 3
+        elif opCode == 7:
+            if (evaluateMode(x, i, 1, paramDict) < evaluateMode(
+                    x, i, 2, paramDict)):
+                x[x[i + 3]] = 1
+            else:
+                x[x[i + 3]] = 0
+            i = i + 4
+        elif opCode == 8:
+            if (evaluateMode(x, i, 1,
+                             paramDict) == evaluateMode(x, i, 2, paramDict)):
+                x[x[i + 3]] = 1
+            else:
+                x[x[i + 3]] = 0
+            i = i + 4
         else:
             raise ValueError("Strange opcode encountered: " + str(x[i]))
 
     return x
 
 
-evaluateCode(opcodeListPartI)
+# evaluateCode(opcodeListPartI)
 
 # Answer for Part One : 13933662 (somehow this messy code works)
 
+# Part 2
 
-# Answer for Part Two : 6979
+opcodeListPartII = opcodeList.copy()
+evaluateCode(opcodeListPartII, 5)
+# Answer for Part Two : 2369720
