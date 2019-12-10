d2c40df22d9be09de079e01f848d7c64392e2b02
Rohith Mohan <Rohith.MOHAN@3ds.com>
2019-12-10 15:22:27 -0800

    Day 8 part 2 solved and passed

--- Day 8/code.py
+++ Day 8/code.py
@@ -28,3 +28,40 @@ print(bestResult)
 
 # Answer for Part One : 1463
 
+# Part 2
+
+# black = 0
+# transparent = 2
+# white = 1
+
+width = 25
+height = 6
+layerDim = width * height
+results = []
+
+for i in range(int(len(data) / (layerDim))):
+    currentLayer = data[layerDim * i:layerDim * (i + 1)]
+    results.append(currentLayer)
+
+finalImageFlat = list('x' * layerDim)
+for layer in results:
+    for pixelIterator in range(len(layer)):
+        pixel = layer[pixelIterator]
+        if finalImageFlat[pixelIterator] == 'x':
+            if pixel == '0':
+                finalImageFlat[pixelIterator] = '0'
+            elif pixel == '1':
+                finalImageFlat[pixelIterator] = '1'
+
+count = 0
+finalImage = ""
+for x in range(height):
+    for y in range(width):
+        finalImage = finalImage + finalImageFlat[count].replace('0', ' ')
+        count += 1
+    finalImage = finalImage + "\n"
+
+print(finalImage)
+
+# Answer for Part Two : GKCKH
+# Needed to replace 0 with a blank string to simulate a white background
